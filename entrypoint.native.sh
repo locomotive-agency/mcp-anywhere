87193ef69c297413157e444ce5f1698059709295
Rob <rob.p.tec@gmail.com>
2025-12-06 15:43:27 +0000

    Added database migrations for older installs

--- entrypoint.native.sh
+++ entrypoint.native.sh
@@ -8,6 +8,7 @@ if sqlite3 .data/mcp_anywhere.db "SELECT * FROM alembic_version;" ; then
 else
     if sqlite3 .data/mcp_anywhere.db "SELECT * FROM users;" ; then
       echo ">>> User database exists, skipping initial migration..."
+      alembic stamp 001 # skip initial migration
       alembic upgrade head
     else
       echo ">>> User database not found. Continuing"
