9bc99adc8a7cd59dd2750fedc5d6d60ce31aa07f
Rob <rob.p.tec@gmail.com>
2025-12-08 17:35:51 +0000

    added db backup

--- entrypoint.native.sh
+++ entrypoint.native.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+DB=".data/mcp_anywhere.db"
+DB_BACKUP_FILE=".data/mcp_anywhere.db.$(date +%s)"
+
+# Backup Database
+echo ">>> Running db backup to ${DB_BACKUP_FILE}"
+sqlite3 ${DB} ".backup ${DB_BACKUP_FILE}"
+echo ">>> backup complete."
+
 # Manage Migrations
 alembic upgrade head
 
