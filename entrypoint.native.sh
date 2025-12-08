c5df972f903c590d4e468639d10212ba61765ea4
Rob <rob.p.tec@gmail.com>
2025-12-08 17:48:37 +0000

    added db backup

--- entrypoint.native.sh
+++ entrypoint.native.sh
@@ -5,7 +5,7 @@ DB=".data/mcp_anywhere.db"
 DB_BACKUP_FILE=".data/mcp_anywhere.db.$(date +%s)"
 
 # Backup Database
-if [ ! -f ${DB} ]; then
+if [ -e ${DB} ]; then
     echo ">>> Running db backup to ${DB_BACKUP_FILE}"
     sqlite3 ${DB} ".backup ${DB_BACKUP_FILE}"
     echo ">>> backup complete."
