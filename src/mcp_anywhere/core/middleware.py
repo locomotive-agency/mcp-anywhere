3f0dd351c6c30f8f066dd9fbd2f54ab16c6371a9
Rob <rob.p.tec@gmail.com>
2025-12-13 19:41:18 +0000

    store user in middleware request

--- src/mcp_anywhere/core/middleware.py
+++ src/mcp_anywhere/core/middleware.py
@@ -42,9 +42,9 @@ class ToolFilterMiddleware(Middleware):
         # Get the tools from the next middleware in the chain
         tools = await call_next(context)
 
-        state = context.fastmcp_context.get_http_request().state
+        user = context.fastmcp_context.get_http_request().state.user["user"]
 
-        logger.info(f"Filtering tools list for req state {state}")
+        logger.info(f"Filtering tools list for req state {user}")
 
         try:
             disabled_tools = await self._get_disabled_tools_async()
